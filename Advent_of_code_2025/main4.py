bd479c479b512f2ad056fedf7384ae1ede98f96d
mxcld99 <77276341+mxcld99@users.noreply.github.com>
2025-12-16 21:54:04 +0000

    saving progress

--- Advent_of_code_2025/main4.py
+++ Advent_of_code_2025/main4.py
@@ -9,42 +9,81 @@ input = os.path.join(os.getcwd(),'input4.txt')
 # Reading content of file
 with open(input) as file:
     read_lines = [i[:-1] for i in file.readlines()]
-    rows = len(read_lines[0])
-    cols = len(read_lines)
-    buffer = list('.'*rows)
+    cols = len(read_lines[0])
+    rows = len(read_lines)
+    buffer = list('.'*(cols+2))
     lines = [list('.'+i+'.') for i in read_lines]
     lines = [buffer]+lines+[buffer]
 
-def check_move(array,row,col):
+###########################
+## Part 1
+###########################
+
+def check_move(array,col,row,debug=False):
     count_rolls = 0
-    for row_diff in range(-1,0,2):
-        for col_diff in range(-1,0,2):
-            if row_diff==0 and col_diff==0:
-                continue
-            row_coord = row+row_diff
+    print_str = ''
+    for row_diff in range(-1,2):
+        for col_diff in range(-1,2):
             col_coord = col+col_diff
-            count_rolls += 1 if array[col_coord][row_coord] == '@' else 0
+            row_coord = row+row_diff
+            char = array[row_coord][col_coord]
+            print_str+=char
+            if col_diff==0 and row_diff==0:
+                continue
+            count_rolls += 1 if array[row_coord][col_coord] == '@' else 0
             if count_rolls >3:
                 return 0  
+        print_str+='\n'
+    if debug:
+        print('\n')
+        print(print_str)
+        print('Roll count: ',count_rolls)
     return 1
 
 
-with open('output.txt','w') as file:
-    total = 0
+total = 0
+for row in range(1,rows+1):
     for col in range(1,cols+1):
-        for row in range(1,rows+1):
-            total += check_move(lines,row,col)
-            file.write(lines[col][row])
-        file.write('\n')
-    print(total)
+        if lines[row][col]!='@':
+            continue
+        total += check_move(lines,col,row,debug=False)
 
-# Checking output 
-with open('output.txt','r') as file1:
-    out_lines = file1.readlines()
+print(total)
 
-with open(input) as file2:
-    in_lines  = file2.readlines()
+###########################
+## Part 2
+###########################
 
-for i,j in zip(out_lines,in_lines):
-    if i!=j:
-        print('mismatch')
+some_removed = True
+current_array = lines
+total_removed = 0
+iteration = 1
+while some_removed:
+    new_array = []
+    removed_this_loop = 0
+    # print(len(current_array[0]))
+    # print(len(current_array))
+    for row in range(1,rows+1):
+        new_row = ['.']
+
+        for col in range(1,cols+1):
+            if current_array[row][col]!='@':
+                new_row.append('.')
+                continue
+            move_check = check_move(current_array,col,row,debug=False)
+            if move_check:
+                new_row.append('.')
+            else:
+                new_row.append('@')
+            removed_this_loop += move_check
+
+        new_row.append('.')
+        new_array.append(new_row.copy())
+
+    current_array = [buffer]+new_array.copy()+[buffer]
+    some_removed = removed_this_loop > 0
+    total_removed+=removed_this_loop
+    print(f'Iteration {iteration} complete. Removed: {removed_this_loop}')
+    iteration += 1
+
+print(total_removed)
